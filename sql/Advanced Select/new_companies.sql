e0e6d8c3633c565fb225a5673e7b777662a8cbe2
Jose A Tula Leyva <joseph.tula@gmail.com>
2017-06-25 11:38:45 -0300

    new_companies

---
+++ sql/Advanced Select/new_companies.sql
@@ -0,0 +1,6 @@
+select e.company_code, c.founder, count(distinct e.lead_manager_code), count(distinct e.senior_manager_code), count(distinct e.manager_code), count(distinct e.employee_code) 
+from employee e
+inner join company c 
+on e.company_code = c.company_code 
+group by e.company_code,c.founder 
+order by e.company_code;
