b0db165f65fb58fabdb3db17f9a3d30dbb0d1cb9
Jose A Tula Leyva <joseph.tula@gmail.com>
2017-07-15 19:58:06 -0300

    insertion sort part2 with loop invariant

--- algorithms/sorting/python/insertionsort_part2.py
+++ algorithms/sorting/python/insertionsort_part2.py
@@ -1,17 +1,19 @@
 #!/bin/python3
-
 import sys
 
 def insertion_sort(a):
+    """
+    Solution to Insertion Part 2 with loop invariant
+    (solution to next exercise - Correctness and the Loop Invariant)
+    """
     for j in range(1, len(a)):
         key = a[j]
         i = j - 1
         while i >= 0 and a[i] > key:
             a[i + 1] = a[i]
             i = i - 1
-        
         a[i + 1] = key
-        print(*a, sep=' ')
+    print(*a, sep=' ')
 
 n = int(input().strip())
 arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
